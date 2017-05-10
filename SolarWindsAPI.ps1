ef5e78ab20e42ebc6ec869ceccaf36b9f5a7fa20
Sean Stark <stark@olympus.F5Net.com>
2017-05-10 15:25:31 -0700

    Major Changes to Get-SWEntities

--- SolarWindsAPI.ps1
+++ SolarWindsAPI.ps1
@@ -53,7 +53,7 @@ function Enable-TrustAllCertificates {
   Get-BasicAuthCreds -Username 'domain\username' -Password 'P@ssw0rd'
 #>
 
-Function New-BasicAuthCreds {
+Function New-SWBasicAuthCreds {
     param(
         [Parameter(Mandatory=$True,
         ValueFromPipeline=$True,
@@ -81,13 +81,13 @@ Function New-BasicAuthCreds {
   .PARAMETER ServerName
   The solarwinds FQDN servername. 
   .PARAMETER Credential
-  Accepts a single Base64Encoded Credential. Use the New-BasicAuthCreds Function to generate a credential
+  Accepts a single Base64Encoded Credential. Use the New-SWBasicAuthCreds Function to generate a credential
   .PARAMETER ServerPort
   Only required if your solarwinds website is not running on the default port of 443
   .EXAMPLE
-  New-SWAPISession -ServerName solarwinds.test.com -Credential $Cred
+  New-SWSession -ServerName solarwinds.test.com -Credential $Cred
   .EXAMPLE
-  New-SWAPISession -ServerName 'solarwinds.test.com' -Credential $Cred -ServerPort '6751'
+  New-SWSession -ServerName 'solarwinds.test.com' -Credential $Cred -ServerPort '6751'
   .NOTES
   Assumes your solarwinds website is using SSL and running on the default port of 443. 
   If you are not running SSL on your solarwinds website you should fix this as credentials are passed in base64encoded clear text
@@ -166,25 +166,25 @@ Function New-SWSession {
 
 <#
   .SYNOPSIS
-  Gets a list of entities from an API endpoint Path
+  Gets a list of entities 
   .DESCRIPTION
-  This is a univeral function that returns a list of objects from the entities API endpoint. 
+  Gets a list of entities from the entities API endpoint. Please read the list of valid paramaters for filtering. 
   .PARAMETER ServerName
   The solarwinds FQDN servername. 
   .PARAMETER WebSession
-  An existing Microsoft.PowerShell.Commands.WebRequestSession. Use the New-SolarWindsSession to generate a web session
+  An existing Microsoft.PowerShell.Commands.WebRequestSession. Use the New-SWSession to generate a web session
   .PARAMETER Endpoint
-  Specify the API Endpoint Path. (entities/states/, entities/types, entities, etc...)
+  Specify the API Endpoint Path. (entities/states/, entities/types, entities, etc...). The default is entities.
   .PARAMETER ServerPort
   Only required if your solarwinds website is not running on the default port of 443
   .PARAMETER Offset
   The offset parameter controls the starting point within the collection of resource results. 
   For example, if you have a collection of 15 items to be retrieved from a resource and you specify Length=5, 
   you can retrieve the entire set of results in 3 successive requests by varying the offset value: offset=0, offset=5, and offset=10. 
-  Note that the first item in the collection is retrieved by setting a zero offset..
+  Note that the first item in the collection is retrieved by setting a zero offset. The Default value is 0.
   .PARAMETER Length
   The Length parameter controls the maximum number of items that may be returned for a single request. 
-  This parameter can be thought of as the page size. If no length is specified, the system defaults to a length of 0 or all records.
+  The Default value is 10. A length value of 0 will return all entities (This will take a while to process). 
   .PARAMETER OrderBy
   The orderBy parameter can be used to order results based on a specific attribute
   .PARAMETER Sort
@@ -194,51 +194,54 @@ Function New-SWSession {
   .PARAMETER status
   The Status parameter can be used to sort results based on the status of an object
   .PARAMETER type
-  The Type parameter can be used to sort results based on the type of an object. This useful for only returning nodes for example. 
-  ?length=20&offset=5&sort=description&type=Orion.Nodes
-  Returns a subset of objects based on an attribute. Expects a string in the format of 'attribute=value'. 
-  Specify multiple attributes seperated by the & symbol 'attribute=value&attribute2=value'
-
-  To see current attributes run Get-Nodes -ShowAttributes
+  The Type ot instanceType parameter can be used to sort results based on the type of an object. For example you can specifcy a type of nodes to only return node entities.
   .PARAMETER ShowAttributes
-  Shows an example of the attributes you can filter on. 
+  Shows an example of the attributes you can filter on. This parameter cannot be used with any other filter paramaters. 
   .EXAMPLE
-  Get All Nodes
-  Get-SWObjects -ServerName solarwinds.test.com -WebSession $WebSession -Path 'entities/'
-  .EXAMPE
-  Get All Metric Types
-  Get-SWObjects -ServerName solarwinds.test.com -WebSession $WebSession -Path 'metrics/'
-   .EXAMPE
-  Get All Metric Types by Group
-  Get-SWObjects -ServerName solarwinds.test.com -WebSession $WebSession -Path 'entities/types/'
-    .EXAMPE
-  Get All States
-  Get-SWObjects -ServerName solarwinds.test.com -WebSession $WebSession -Path 'entities/states/'
+  In the below examples the variable $WebSession was created using the New-SWSession cmdlet
   .EXAMPLE
-  Get Top 5 nodes
-  Get-SWObjects -ServerName solarwinds.test.com -WebSession $WebSession -Path 'entities/' -Limit 5
+  Get the first 10 entities
+  $WebSession | Get-SWEntities
   .EXAMPLE
-  Using the Filter Paramater
+  The below examples work with returning a limited number of records using the Length and Offset parameters
 
-  Get All Nodes that are down
-  Get-SWObjects -ServerName solarwinds.test.com -WebSession $WebSession -Filter -Path 'entities/' 'status=2'
+  $WebSession | Get the first 50 entities 
+  $WebSession | Get-SWEntities -Length 50
 
-  Get a single node based on displayname
-  Get-SWObjects -ServerName solarwinds.test.com -WebSession $WebSession -Filter -Path 'entities/' 'displayName=test.internal.net'
+  Get the first 50 entities with an offset of 2
+  $WebSession | Get-SWEntities -Length 50 -Offset 2
+  .EXAMPLE
+  The below examples work with returning a entities based on a type.
 
-  Get nodes based on a Like Operator using %. Below will return all nodes with srv in the displayName
-  Get-SWObjects -ServerName solarwinds.test.com -WebSession $WebSession -Path 'entities/' -Filter 'displayName=%srv%'
+  Get the first 10 entities that are nodes 
+  $WebSession | Get-SWEntities -type Orion.Nodes
 
-  Get nodes based on multiple filters. Below will return all nodes with srv in the displayName and have windows in the description
-  Get-SWObjects -ServerName solarwinds.test.com -WebSession $WebSession -Path 'entities/' -Filter 'displayName=%srv%&description=%windows%'
+  Get all entities that are nodes
+  $WebSession | Get-SWEntities -type Orion.Nodes -Length 0
 
+  Get all entities that are Exchange APM Applications
+  $WebSession | Get-SWEntities -type Orion.APM.Exchange.Application
+  .EXAMPLE
+  The below examples work with returning a entities based on a status. 
+
+  Get the first 10 entities that are down
+  $WebSession | Get-SWEntities -status 2
+
+  .EXAMPLE
+  The below examples sort and OrderBy the records returned. 
+
+  Get the first 100 nodes, sort by status
+  $WebSession | Get-SWEntities -type Orion.Nodes -sort status
+
+  Get the first 100 nodes, groupby description
+  $WebSession | Get-SWEntities -type Orion.Nodes -OrderBy description
 
 #>
 
-Function Get-SWObjects{
+Function Get-SWEntities{
     [CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Low',DefaultParametersetName='ParamDefault')]
     param(
-        [Parameter(Mandatory=$false,
+        [Parameter(Mandatory=$true,
         ValueFromPipeline=$True,
         ValueFromPipelineByPropertyName=$True)]
         [ValidateNotNull()]
@@ -246,56 +249,81 @@ Function Get-SWObjects{
         [Alias('SolarWindsServer')]
         [string]$ServerName,
         
-        [Parameter(Mandatory=$false,
-        ValueFromPipeline=$True,
-        ValueFromPipelineByPropertyName=$True)]
-        [Alias('Port')]
-        [string]$ServerPort = '443',
-
-        [Parameter(Mandatory=$false,
+        [Parameter(Mandatory=$true,
         ValueFromPipeline=$True,
         ValueFromPipelineByPropertyName=$True)]
         [ValidateNotNull()]
         [Microsoft.PowerShell.Commands.WebRequestSession]$WebSession,
 
-        [Parameter(Mandatory=$True,
+        [Parameter(ValueFromPipeline=$True,
         ValueFromPipelineByPropertyName=$True)]
+        [Alias('Port')]
+        [int]$ServerPort = '443',
+
+        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName = 'ParamQuery')]
         [ValidateNotNull()]
-        [string]$EndPoint,
+        [string]$EndPoint = 'entities',
+
+        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName = 'ParamQuery')]
+        [int]$Offset = 0,
+
+        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName = 'ParamQuery')]
+        [int]$Length = 10,
+
+        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName = 'ParamQuery')]
+        [string]$OrderBy,
+
+        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName = 'ParamQuery')]
+        [string]$Sort,
+
+        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName = 'ParamQuery')]
+        [string]$DisplayName,
+
+        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName = 'ParamQuery')]
+        [int]$Status,
+
+        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName = 'ParamQuery')]
+        [string]$Type,
 
         [Parameter(ParameterSetName = 'ParamShowAtrribs')]
-        [switch]$ShowAttributes = $false,
-
-        [int]$Limit = 0,
-
-        [Parameter(ParameterSetName = 'ParamFilterAtrribs')]
-        [string]$Filter
+        [switch]$ShowAttributes = $false
     )
 
     begin {
        
-        Write-Verbose "Parameter Set: $($PSCmdlet.ParameterSetName)"
+        # Originally had the Endpoint cleanup up in here, however ValueFromPipelineByPropertyName and ValueFromPipeline doesn't get proccessed in the begin block. 
+        # Didn't feel like going through the trouble of creating a $InputObject, leaving this in place for notes
     }
 
     process {
-        
-        Write-Verbose $Temp.Websession.headers
 
         #Cleanup Path Variable
         $EndPoint = $EndPoint.Replace('//','/')
 
-        Write-Verbose 'Formatting ServerName to Base solarwinds address for establishing a session'
-        
-        $URI = "$($ServerName):$($ServerPort)/$global:APIRootPath/$EndPoint/?length=$Limit&offset=0"
-        
-        If($Filter){
-            Write-Verbose 'Filter Parameter Was Specified, Formatting URI'
-            $URI = "$($ServerName):$($ServerPort)/$global:APIRootPath/$EndPoint/?length=$Limit&offset=0$Filter"
-        }
+        If(!($ShowAttributes)){
+            
+            Write-Verbose $PSBoundParameters.GetEnumerator()
+            $URIParams = @{} 
 
-        If($ShowAttributes){
+            #Adding Parameters to URIParams
+            $URIParams.add('Length', $Length)
+            $URIParams.add('Offset', $Offset)
+            If ($DisplayName){$URIParams.add('DisplayName', $DisplayName)}
+            If ($PSBoundParameters.ContainsKey('Status')){$URIParams.add('Status', $status)}
+            If ($Type){$URIParams.add('Type', $type)}
+            If ($OrderBy){$URIParams.add('OrderBy', $OrderBy)}
+            If ($Sort){$URIParams.add('Sort', $Sort)}
+
+            #Format URIParams to single line
+            $URIParams = ($URIParams.GetEnumerator() | ForEach-Object { "$($_.Key)=$($_.Value)"}) -join '&'
+
+            Write-Verbose $URIParams
+
+            $URI = "$($ServerName):$($ServerPort)/$global:APIRootPath/$EndPoint/?$URIParams"
+        }
+        Else{
             Write-Verbose 'ShowAttributes Parameter Was Specified, Formatting URI'
-            $URI = "$($ServerName):$($ServerPort)/$global:APIRootPath/$EndPoint/?length=$Limit&offset=0"
+            $URI = "$($ServerName):$($ServerPort)/$global:APIRootPath/$EndPoint/?length=1"
         }
 
         #Cleanup URI  Variable
@@ -304,15 +332,12 @@ Function Get-SWObjects{
         try{
             Write-Verbose "Attempting Connection to $URI"
             If ($pscmdlet.ShouldProcess($URI)){
-
                 $Request = Invoke-RestMethod -Method Get -Uri $URI -ContentType 'application/json' -WebSession $WebSession
             }
 
         }
         catch{
-
             Write-Error $_
-
         }
 
     }
@@ -322,141 +347,7 @@ Function Get-SWObjects{
             $Request = $Request.data
         }
 
-        return $Request
-    }
-}
-
-<#
-  .SYNOPSIS
-  Gets a list of Nodes from the SolarWinds API
-  .DESCRIPTION
-  Returns a list of nodes from the /api2/perfstack/entities API endpoint uses the filter type=Orion.Nodes to only display nodes
-  .PARAMETER ServerName
-  The solarwinds FQDN servername. 
-  .PARAMETER WebSession
-  An existing Microsoft.PowerShell.Commands.WebRequestSession. Use the New-SolarWindsSession to generate a web session
-  .PARAMETER ServerPort
-  Only required if your solarwinds website is not running on the default port of 443
-  .PARAMETER Limit
-  Returns a subset of objects from the request. Expects an integer ranging from 0 to unkown number. Default value is 0 which returns all nodes.
-  You cannot specify the Filter or ShowAttributes parameters with this parameter.
-  .PARAMETER Filter
-  Returns a subset of objects based on an attribute. Expects a string in the format of 'attribute=value'. 
-  Specify multiple attributes seperated by the & symbol 'attribute=value&attribute2=value'
-  To see current attributes run Get-Nodes -ShowAttributes
-  You cannot specify the Limit or ShowAttributes parameters with this parameter. 
-  .PARAMETER ShowAttributes
-  Shows an example of the attributes you can filter on. You cannot specify the Limit or Filter parameters with this parameter. 
-  .PARAMETER Endpoint
-  Should only be used for development purposes. Specifies the nodes API endpoint, default is entities/
-  .EXAMPLE
-  Get All Nodes
-  Get-SWNodes -ServerName solarwinds.test.com -WebSession $WebSession
-  .EXAMPLE
-  Get Top 5 nodes
-  Get-SWNodes -ServerName solarwinds.test.com -WebSession $WebSession -Limit 5
-  .EXAMPLE
-  Using the Filter Paramater
-
-  Get All Nodes that are down
-  Get-SWNodes -ServerName solarwinds.test.com -WebSession $WebSession -Filter 'status=2'
-
-  Get a single node based on displayname
-  Get-SWNodes -ServerName solarwinds.test.com -WebSession $WebSession -Filter 'displayName=test.internal.net'
-
-  Get nodes based on a Like Operator using %. Below will return all nodes with srv in the displayName
-  Get-SWNodes -ServerName solarwinds.test.com -WebSession $WebSession -Filter 'displayName=%srv%'
-
-  Get nodes based on multiple filters. Below will return all nodes with srv in the displayName and have windows in the description
-  Get-SWNodes -ServerName solarwinds.test.com -WebSession $WebSession -Filter 'displayName=%srv%&description=%windows%'
-
-#>
-
-Function Get-SWNodes{
-    [CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Low',DefaultParametersetName='ParamDefault')]
-    param(
-        [Parameter(Mandatory=$false,
-        ValueFromPipeline=$True,
-        ValueFromPipelineByPropertyName=$True)]
-        [ValidateNotNull()]
-        [Alias('Server')]
-        [Alias('SolarWindsServer')]
-        [string]$ServerName,
-        
-        [Parameter(Mandatory=$false,
-        ValueFromPipeline=$True,
-        ValueFromPipelineByPropertyName=$True)]
-        [Alias('Port')]
-        [string]$ServerPort = '443',
-
-        [Parameter(Mandatory=$false,
-        ValueFromPipeline=$True,
-        ValueFromPipelineByPropertyName=$True)]
-        [ValidateNotNull()]
-        [Microsoft.PowerShell.Commands.WebRequestSession]$WebSession,
-
-        [Parameter(ParameterSetName = 'ParamShowAtrribs')]
-        [switch]$ShowAttributes = $false,
-
-        [Parameter(ParameterSetName = 'ParamLimit')]
-        [int]$Limit = 0,
-
-        [Parameter(ParameterSetName = 'ParamFilterAtrribs')]
-        [string]$Filter,
-
-        [Alias('Path')]
-        [string]$Endpoint = 'entities/'
-
-    )
-
-    begin {
-
-    }
-
-    process {
-
-        #Cleanup Path Variable
-        $EndPoint = ($EndPoint + '/').Replace('//','/')
-
-        Write-Verbose 'Formatting ServerName to Base solarwinds address for establishing a session'
-        
-        $URI = "$($ServerName):$($ServerPort)/$global:APIRootPath/$EndPoint/?length=$Limit&offset=0&type=Orion.Nodes"
-        
-        If($Filter){
-            Write-Verbose 'Filter Parameter Was Specified, Formatting URI'
-            $URI = "$($ServerName):$($ServerPort)/$global:APIRootPath/$EndPoint/?$Filter&offset=0&type=Orion.Nodes"
-        }
-
-        If($ShowAttributes){
-            Write-Verbose 'ShowAttributes Parameter Was Specified, Formatting URI'
-            $URI = "$($ServerName):$($ServerPort)/$global:APIRootPath/$EndPoint/?length=1&offset=0&type=Orion.Nodes"
-        }
-
-        #Cleanup URI  Variable
-        $URI  = "https://$($URI.Replace('//','/'))"
-
-        try{
-            Write-Verbose "Attempting Connection to $URI"
-            If ($pscmdlet.ShouldProcess($URI)){
-
-                $Request = Invoke-RestMethod -Method Get -Uri $URI -ContentType 'application/json' -WebSession $WebSession
-            }
-
-        }
-        catch{
-
-            Write-Error $_
-
-        }
-
-    }
-    End{
-        #Only Return Filterable Attributes if specified
-        If($ShowAttributes){
-            $Request = $Request.data
-        }
-
-        return $Request
+        return $Request.data
     }
 }
 
